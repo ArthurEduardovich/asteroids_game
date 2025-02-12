7c96596090c2c6113aede52ea74adce97f4a3050
ArthurEduardovich <artyr.eduardovich@gmail.com>
2025-02-12 22:05:10 +0300

    lesson 5 completed

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
+
+        screen.fill(BLACK) # Заполнение экрана чёрным цветом (фон)
+        player.draw(screen)
+        pygame.display.flip() # Обновление экрана
+
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
